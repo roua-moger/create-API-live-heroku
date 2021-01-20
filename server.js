3ba87e797580dea41b1e0101910976b2581a575c
Rowaa <rowaa.almogheer@gmail.com>
2021-01-20 09:22:05 +0100

    first commet

---
+++ server.js
@@ -0,0 +1,17 @@
+// var express = require('express');
+// var app = express();
+// var fs = require('fs');
+
+// app.get('/contacts', (req, res) => {
+//     fs.readFile(__dirname + "/" + "./contacts.json" , 'utf8', (err, data) => {
+//         console.log(data);
+//         res.send(data);
+//     });
+// });
+
+// var server = app.listen( 8080 , () => {
+//     var host = server.address().address
+//     var port = server.address().port
+//     console.log("REST API demo app listening at http://%s:%S", host, port)
+// })
+
